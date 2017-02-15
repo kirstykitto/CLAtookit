3161e170764ef3a27b0c8ffeb866d6290ff37e90
Zak <zak.waters@connect.qut.edu.au>
2017-02-15 10:41:06 +1000

    Adding new Front-page/Registration flow
    Main changes:
    - New beautiful front page
    - Registration flow that retrieves social data automatically with the
    help of Vue.js

--- clatoolkit_project/clatoolkit/migrations/0001_squashed_0002_auto_20170206_0521.py
+++ clatoolkit_project/clatoolkit/migrations/0001_squashed_0002_auto_20170206_0521.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django_pgjson.fields
+import datetime
+from django.utils.timezone import utc
 from django.conf import settings
 
 
@@ -226,5 +228,29 @@ class Migration(migrations.Migration):
             model_name='cachedcontent',
             name='unit',
             field=models.ForeignKey(to='clatoolkit.UnitOffering'),
+        ),
+        migrations.AddField(
+            model_name='dashboardreflection',
+            name='unit',
+            field=models.ForeignKey(default=1, to='clatoolkit.UnitOffering'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='learningrecord',
+            name='datetimestamp',
+            field=models.DateTimeField(default=datetime.datetime(2017, 2, 6, 5, 21, 4, 225460, tzinfo=utc)),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='unitoffering',
+            name='end_date',
+            field=models.DateField(default=datetime.datetime(2017, 2, 6, 5, 21, 8, 763640, tzinfo=utc)),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='unitoffering',
+            name='start_date',
+            field=models.DateField(default=datetime.datetime(2017, 2, 6, 5, 21, 13, 373698, tzinfo=utc)),
+            preserve_default=False,
         ),
     ]
